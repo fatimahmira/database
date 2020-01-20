9edaf058d006f582bdb2a7c98c6a935de300c7f7
fatimahmira <49262430+fatimahmira@users.noreply.github.com>
2020-01-20 10:31:50 +0700

    a

---
+++ lib/SecondScreen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
-import 'package:database/adminpage.dart';
-import 'package:database/memberpage.dart';
+import 'package:database/SecondScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -9,13 +8,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: ConnectDatabase(),
-    theme: ThemeData(),
-    routes: <String,WidgetBuilder>{
-      '/AdminPage' : (BuildContext context) => new AdminPage(),
-      '/MemberPage' : (BuildContext context) => new MemberPage(),
-      
-       
+    // home: ConnectDatabase(),
+    // initialRoute: '/',
+    // theme: ThemeData(),
+    routes: <String, WidgetBuilder>{
+      '/' : ( context) =>  ConnectDatabase(),
+      '/second' : (context) => SecondScreen(),
+      // '/AdminPage': ( context) =>  AdminPage(),
+      // '/MemberPage': ( context) =>  MemberPage(),
     },
   ));
 }
@@ -31,9 +31,9 @@ class _ConnectDatabaseState extends State<ConnectDatabase> {
   LoginStatus _loginStatus = LoginStatus.notSignIn;
   TextEditingController contEmail = new TextEditingController();
   TextEditingController contPassword = new TextEditingController();
-  
-  String msg='';
-  
+
+  String msg = '';
+
   final _key = new GlobalKey<FormState>();
 
   bool _secureText = true;
@@ -55,85 +55,54 @@ class _ConnectDatabaseState extends State<ConnectDatabase> {
   Future<List> login() async {
     String email = contEmail.text;
     String password = contPassword.text;
-    final response = await http.post("http://172.16.141.33/Flutter/login.php",
-        body: {
-        "email": email, 
-        "password": password 
-        });
+    final response = await http.post("http://172.20.10.3/Flutter/login.php",
+        body: {"email": email, "password": password});
 
-    var data = jsonDecode(response.body);    
+    var data = json.decode(response.body);
 
-    int value = data['value'];
-    String pesan = data['message'];
-    // String password = data['password'];
-    String emailAPI = data['email'];
-    int level = data['level'];
-    // int id = data['id'];
-    // print(data.toString());
-    if (value == 1) {
-      setState(() {
-        _loginStatus = LoginStatus.signIn;
-        // savePref(value, emailAPI, namaAPI, id);
-        if(data['level']==1){
-        print("welcome Favian");
-      } else if (data['level']==2){
-        print("welcome mir");
-      }
-      });
-      // print(data[0]['level'].toString());
-      // print(level);
-      print(pesan);
-      print(emailAPI);
-      print(password);
+    if (data.length == 0) {
+      print("login failed");
     } else {
-      print(pesan);
-      print(emailAPI);
-      print(password);
+      setState(() {
+        //     // _loginStatus = LoginStatus.signIn;
+        //     // savePref(value, emailAPI, namaAPI, id);
+        if (data[0]['level'] == '1') {
+          // return ("welcome");
+          // print("welcome Favian");
+          // Navigator.pushReplacementNamed(context, ("/AdminPage"));
+        } else if (data[0]['level'] == '2') {
+          // return ("welcome2");
+          // print("welcome mir");
+          Navigator.pushNamed(context, '/second');
+          // Navigator.pushReplacementNamed(context, "/MemberPage");
+        } else {
+          print("welocme nope");
+        }
+      });
     }
-  }
-
-
-  savePref(int value, String email, String nama, String id) async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    setState(() {
-      preferences.setInt("value", value);
-      preferences.setString("nama", nama);
-      preferences.setString("email", email);
-      preferences.setString("id", id);
-      preferences.commit();
-    });
-  }
-
-  var value;
-  getPref() async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    setState(() {
-      value = preferences.getInt("value");
-
-      _loginStatus = value == 1 ? LoginStatus.signIn : LoginStatus.notSignIn;
-    });
-  }
-
-  signOut() async {
-    SharedPreferences preferences = await SharedPreferences.getInstance();
-    setState(() {
-      preferences.setInt("value", null);
-      preferences.commit();
-      _loginStatus = LoginStatus.notSignIn;
-    });
+    // });
+    // print(data[0]['level'].toString());
+    // print(level);
+    //   print(pesan);
+    //   print(emailAPI);
+    //   print(password);
+    // } else {
+    //   print(pesan);
+    //   print(emailAPI);
+    //   print(password);
+    // }
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getPref();
   }
 
   @override
   Widget build(BuildContext context) {
-    switch (_loginStatus) {
-      case LoginStatus.notSignIn:
+    // switch (_loginStatus) {
+    //   case LoginStatus.notSignIn:
         return Scaffold(
           appBar: AppBar(
             title: Text('Login'),
@@ -173,78 +142,18 @@ class _ConnectDatabaseState extends State<ConnectDatabase> {
                   },
                   child: Text("Login"),
                 ),
-
-                Text(msg,style: TextStyle(fontSize: 20, color: Colors.red),)
+                Text(
+                  msg,
+                  style: TextStyle(fontSize: 20, color: Colors.red),
+                )
               ],
             ),
           ),
         );
-        break;
-      case LoginStatus.signIn:
-        return MainMenu(signOut());
-        break;
-    }
+      //   break;
+      // case LoginStatus.signIn:
+      //   // return MainMenu(signOut());
+      //   break;
+    
   }
 }
-
-class MainMenu extends StatefulWidget {
-  final VoidCallback signOut;
-  MainMenu(this.signOut);
-  @override
-  _MainMenuState createState() => _MainMenuState();
-}
-
-class _MainMenuState extends State<MainMenu> {
- signOut() {
-   setState(() {
-     widget.signOut();
-   });
- }
-
- getPref() async {
-   SharedPreferences preferences = await SharedPreferences.getInstance();
-   setState(() {
-    //  email = preferences.getString("email");
-    //  nama = preferences.getString("nama");
-   });
- }
-
- @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getPref();
-  }
-
-  Widget build (BuildContext context){
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-         title: Text("Halaman Dashboard"),
-         actions: <Widget>[
-           IconButton(
-             onPressed: () {
-               signOut();
-             },
-             icon: Icon(Icons.lock_open),
-           )
-         ],
-        
-        ),
-
-        body: Center(
-         child: Text(
-           "Dashboard"
-         )
-       )
-
-      ),
-      
-      );
-  }
-
-
-
-}
-
